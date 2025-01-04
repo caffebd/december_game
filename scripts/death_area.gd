d844ae1e0f327504b12d33b469cc44d36fbf8444
caffebd <caffebd@gmail.com>
2025-01-04 09:19:57 +0000

    Tile fixes by Luke

--- scripts/death_area.gd
+++ scripts/death_area.gd
@@ -3,4 +3,8 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		print(body.name)
-		get_tree().reload_current_scene()
+		call_deferred("_do_reload")
+
+
+func _do_reload():
+	get_tree().reload_current_scene()
